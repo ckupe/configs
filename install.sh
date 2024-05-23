6f1c3338b00791ca0d4575b07fbb733e2d71cbb8
Chris Kuperstein <chris@kuperstein.net>
2024-05-23 20:46:29 +0000

    adding desktop changes

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
 #!/bin/env bash
 
-sudo rpm-ostree install --apply-live alacritty zsh tmux
-/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-podman pull quay.io/ckuperst/toolbox:latest
-
-toolbox create -i quay.io/ckuperst/toolbox:latest default
+sudo rpm-ostree install -ry --idempotent alacritty zsh tmux
