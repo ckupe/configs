c457b1a7eb3c1a73818002a98aa55ab65d734572
Chris Kuperstein <chris@kuperstein.net>
2024-05-23 17:03:00 -0700

    adding vdpau and vaapi packages

--- ostree/desktop-packages.sh
+++ ostree/desktop-packages.sh
@@ -1,4 +1,10 @@
 #!/bin/env bash
-sudo rpm-ostree install -y --idempotent akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda
-sudo rpm-ostree kargs --reboot --append-if-missing=rd.driver.blacklist=nouveau --append-if-missing=modprobe.blacklist=nouveau --append-if-missing=nvidia-drm.modeset=1
+sudo rpm-ostree install -y --idempotent \
+	akmod-nvidia \
+	xorg-x11-drv-nvidia \
+	xorg-x11-drv-nvidia-cuda \
+	nvidia-vaapi-driver \
+	libva-utils \
+	vdpauinfo
 
+sudo rpm-ostree kargs --reboot --append-if-missing=rd.driver.blacklist=nouveau --append-if-missing=modprobe.blacklist=nouveau --append-if-missing=nvidia-drm.modeset=1
